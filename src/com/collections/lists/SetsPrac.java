43a44fb5fef87026aff5a8223e2617f9e9216a15
hargovindsinghrawat <hargovindsinghrwt@gmail.com>
2024-01-03 18:38:58 +0530

    PriorityQueue

--- src/com/collections/lists/SetsPrac.java
+++ src/com/collections/lists/SetsPrac.java
@@ -1,13 +1,33 @@
 package com.collections.lists;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class SetsPrac {
 
 	public static void main(String[] args){
 	
-		Set<Integer> st = new HashSet<>();
+		/*
+		Set<Integer> set = new HashSet<>();
+		set.add(1);
+		set.add(3);
+		set.add(5);
+		set.add(4);
+		set.add(8);
+		set.remove(2);
+	
+		System.out.println(set);
+		*/
 		
+		Set<Integer> set = new LinkedHashSet<>();
+		set.add(1);
+		set.add(3);
+		set.add(5);
+		set.add(4);
+		set.add(8);
+		set.remove(3);
+	
+		System.out.println(set);
 	}
 }
