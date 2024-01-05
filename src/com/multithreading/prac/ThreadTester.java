cd095c1f829992a52cfe6ca568bf0d0048de2054
hargovindsinghrawat <hargovindsinghrwt@gmail.com>
2024-01-06 02:18:22 +0530

    Synchronization

--- src/com/multithreading/prac/ThreadTester.java
+++ src/com/multithreading/prac/ThreadTester.java
@@ -4,15 +4,39 @@ public class ThreadTester {
 
 	public static void main(String[] args) {
 			
-		Thread td = new Thread1("Thread1");
-//		td.setDaemon(true);
-		td.start();
+//		Thread td = new Thread1("Thread1");
+////		td.setDaemon(true);
+//		td.start();
+//		
+//		System.out.println("main is starting");
+//		
+//		Thread td2 = new Thread(new Thread2() , "thread2");
+//		td2.start();
+//		
+//		Thread td3 = new Thread(() -> {
+//			for(int i=0;i<5;i++) {
+//				System.out.println(Thread.currentThread().getName() + " " + i);
+//			}
+//		}, "thread3");
+//		td3.start();
 		
-		System.out.println("main is starting");
+		Stack stack = new Stack(5);
 		
-		Thread td2 = new Thread(new Thread2() , "thread2");
-		td2.start();
+		new Thread(() -> {
+			int counter = 0;
+			while(++ counter < 0) {
+				System.out.println("Pushed: " + stack.push(100));
+			}
+		}, "Pusher").start();
+		
+		new Thread(() -> {
+			int counter = 0;
+			while(++ counter < 0) {
+				System.out.println("Popped: " + stack.pop());
+			}
+		}, "Popper").start();
+		
 		
 		System.out.println("main is existing");
-	}
+	} 
 }
